7e19aa5cdea9052cd9b65aa235820f121b9144ed
Oleg <slame1121@gmail.com>
2017-04-09 15:26:18 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@ var app = {
 	initialize: function() {
 		var self = this;
 		this.store = new MemoryStore(function() {
+			self.showAlert('Store Initialized', 'Info'); 
 			$('body').html(new HomeView(self.store).render().el);
 		});
-	}
+	},
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
 };
 
 app.initialize();
